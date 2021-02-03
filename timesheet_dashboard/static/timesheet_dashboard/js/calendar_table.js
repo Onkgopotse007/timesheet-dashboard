7c6b5193b3c2eb0fc599bdf1935767ebb6c3afa7
ame diphoko <diphokoame@gmail.com>
2021-02-03 14:25:22 +0200

    update calendar js

--- timesheet_dashboard/static/timesheet_dashboard/js/calendar_table.js
+++ timesheet_dashboard/static/timesheet_dashboard/js/calendar_table.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     $("#add-row").click(function(){
         var task = $("#task").val();
-        var markup = "<tr>"+
-        				"<td> <input id='tasks' type='text' class='form-control form-control-sm' name='tasks' value='" + task + "' readonly /></td>"+
-        				"<td> <input id='dailyentry_set-0-duration' type='number' class='form-control form-control-sm' name='dailyentry_set-0-duration'/> </td>"+
+        
+        var markup =
+        			"<tr>"+
+        				"<td></td>"+
+        				"<td> <input id='dailyentry_set-0-duration' type='number' class='form-control form-control-sm' name='dailyentry_set-0-duration'/></td>"+
         				"<td> <input id='dailyentry_set-1-duration' type='number' class='form-control form-control-sm' name='dailyentry_set-1-duration'/></td>"+
         				"<td> <input id='dailyentry_set-2-duration' type='number' class='form-control form-control-sm' name='dailyentry_set-2-duration'/></td>"+
         				"<td> <input id='dailyentry_set-3-duration' type='number' class='form-control form-control-sm' name='dailyentry_set-3-duration'/></td>"+
@@ -17,9 +19,8 @@ $(document).ready(function(){
         				"<td>0</td>"+
         				"<td></td>"+
         				"<td><a class='btn btn-default btn-sm remove'><i class='glyphicon glyphicon-remove'></i></a></td>"
-        				"</tr>";
+        			"</tr>";
         $("table tbody").append(markup);
-        $("#addModal").modal('hide');
     });
     
 });
@@ -30,46 +31,11 @@ $(document).on('click', '.remove', function() {
 });
 
 $(document).on('click', '#nextWeek', function() {
+	document.getElementById("controller").value = "next";
 	$('#controlForm').submit();
 });
 
-$('#controlForm').on('submit', function (e) {
- e.preventDefault();
- var url = nextWeek();
-console.log(url);
- $.ajax({
- 		url: url,
-      type: "POST",
-      data: $('form').serialize(),
-      success : function(json) {
-        alert("Successfully sent the URL to Django");
-      },
-      error : function(xhr,errmsg,err) {
-        alert("Could not send URL to Django. Error: " + xhr.status + ": " + xhr.responseText);
-      }
-	});
+$(document).on('click', '#prevWeek', function() {
+	document.getElementById("controller").value = "prev";
+	$('#controlForm').submit();
 });
-
-function nextWeek(){
-	var js_variable = JSON.parse(document.getElementById('currDate').textContent);
-	var date = new Date(js_variable);
-	var nextWeekStart = date.getDate() - date.getDay() + 8;
-	var nextWeekFrom = new Date(date.setDate(nextWeekStart));
-	
-    var id = JSON.parse(document.getElementById('employee_id').textContent);
-    return "{% url 'timesheet_dashboard:timesheet_calendar_table_url' 123 %}".replace('123', id) + "/" + convertDate(nextWeekFrom);
-}
-
-Date.prototype.getNextWeekDay = function(d) {
-  if (d) {
-    var next = this;
-    next.setDate(this.getDate() - this.getDay() + 7 + d);
-    return next;
-  }
-}
-
-function convertDate(inputFormat) {
-  function pad(s) { return (s < 10) ? '0' + s : s; }
-  var d = new Date(inputFormat)
-  return [d.getFullYear(), pad(d.getMonth()+1), pad(d.getDate())].join('/')
-}
